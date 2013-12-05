8b5df7e085e75f123d694dbe040324977be73f9a
John Crygier <john.crygier@aon.com>
2013-12-05 13:56:45 -0600

    Testing, version bump.
    Reword some messages.

--- src/main/groovy/org/aon/esolutions/build/tools/gradle/JaCoCoResultsPluginExtension.groovy
+++ src/main/groovy/org/aon/esolutions/build/tools/gradle/JaCoCoResultsPluginExtension.groovy
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2013 Aon eSolutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.aon.esolutions.build.tools.gradle
 
 import org.gradle.api.Project
@@ -27,10 +42,10 @@ class JaCoCoResultsPluginExtension {
         coverageType = CoverageTypes.getValue(type);
 
         if (coverageLevel == null)
-            throw new IllegalArgumentException("jacocoResults.aggregationLevel must be one of: " + CoverageLevels.values() + " but was $coverageLevel");
+            throw new IllegalArgumentException("jacocoResults.aggregationLevel must be one of: " + CoverageLevels.values() + " but was $aggregationLevel");
 
         if (coverageType == null)
-            throw new IllegalArgumentException("jacocoResults.type must be one of: " + CoverageTypes.values() + " but was $coverageType");
+            throw new IllegalArgumentException("jacocoResults.type must be one of: " + CoverageTypes.values() + " but was $type");
 
         if (!coverageLevel.isCoverageTypePermitted(coverageType))
             throw new IllegalArgumentException("jacocoResults.type must be one of: " + coverageLevel.getPermittedCoverageTypes() + " if aggregationLevel is $coverageLevel");
--- src/main/groovy/org/aon/esolutions/build/tools/gradle/tasks/JaCoCoResultsTask.groovy
+++ src/main/groovy/org/aon/esolutions/build/tools/gradle/tasks/JaCoCoResultsTask.groovy
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2013 Aon eSolutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.aon.esolutions.build.tools.gradle.tasks
 
 import groovy.transform.ToString
@@ -64,7 +79,15 @@ class JaCoCoResultsTask extends DefaultTask {
                 allCounters.addAll coverageReport.package."class".collect(this.&transformNodeToCounter).findAll { it != null }
             } else if (CoverageLevels.Method == extension.getCoverageLevel()) {
                 // Add Counters for Method
-                allCounters.addAll coverageReport.package."class".method.collect(this.&transformNodeToCounter).findAll { it != null }
+                allCounters.addAll coverageReport.package."class".collect {
+                    String namePrefix = it.attributes()['name'] + "."
+
+                    return it.method.collect {
+                        this.transformNodeToCounter(it, namePrefix)
+                    }.findAll { it != null }
+                }.flatten()
+
+
             }
         }
     }
@@ -76,10 +99,10 @@ class JaCoCoResultsTask extends DefaultTask {
             return counterNode.@type == extension.getCoverageType().name().toUpperCase()
     }
 
-    def transformNodeToCounter(def parentNode) {
+    def transformNodeToCounter(def parentNode, String namePrefix = "") {
         def counter = parentNode.childNodes().find(this.&isCounterApplicable)
         if (counter)
-            return new Counter(counter, extension, parentNode.@name.text())
+            return new Counter(counter, extension, namePrefix + parentNode.@name.text())
         else
             return null;
     }
